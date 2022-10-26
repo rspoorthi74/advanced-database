6d9e270b0e41610b7aa43a46e327e4c1d30da496
rspoorthi74 <rspoorthi74@gmail.com>
2022-10-26 04:30:27 +0000

    first web app

--- bottle_app.py
+++ bottle_app.py
@@ -1,10 +1,29 @@
+from bottle import default_app, route,template
+import sqlite3
 
-# A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route
+connection= sqlite3.connect("Travel_list.db")
+
 
 @route('/')
 def hello_world():
-    return 'Hello from  Spoorthi!'
+    return 'Hello from Spoorthi!'
 
-application = default_app()
+@route('/hi')
+def hi_world():
+    return 'Hi from Spoorthi!'
 
+@route('/bye')
+def bye_world():
+    return 'Bye from Spoorthi!'
+
+@route('/list')
+def get_list():
+    cursor = connection.cursor()
+    rows = cursor.execute("select id, description from list")
+    rows = list(rows)
+    rows = [ {'id':row[0] , 'desc':row[1]} for row in rows]
+    return template("Travel_list.tpl",name="Spoorthi",Travel_list=rows)
+
+
+
+application =  default_app()
---
+++ setup.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+# DB-API spec for talking to relational databases in Python
+
+connection = sqlite3.connect("Travel_list.db")
+
+cursor = connection.cursor()
+
+try:
+    cursor.execute("drop table list")
+except:
+    pass
+
+cursor.execute("create table list (id integer primary key, description text)")
+
+cursor.execute("insert into list (description) values ('shirts')")
+cursor.execute("insert into list (description) values ('tshirts')")
+cursor.execute("insert into list (description) values ('pants')")
+cursor.execute("insert into list (description) values ('shorts')")
+cursor.execute("insert into list (description) values ('toiletries')")
+cursor.execute("insert into list (description) values ('utensils')")
+
+connection.commit()
+connection.close()
+
+print("done.")
